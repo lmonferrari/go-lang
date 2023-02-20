84094a02640b38646b7963a18487dd13dbe99946
Luiz Monferrari <lmonferrari@outlook.com>
2023-02-20 11:18:09 -0300

    adicionando localização de ip por host

--- curso_1/aplicacao/app/app.go
+++ curso_1/aplicacao/app/app.go
@@ -1,12 +1,44 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Gerar vai retornar a aplicação de linha de comando pronta pra ser executada
 func Gerar() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicaçäo de linha de comando"
 	app.Usage = "Busca IPs e Nomes de Servidor na internet"
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca IPs de endereços na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "deeplearningbook.com.br",
+				},
+			},
+			Action: func(c *cli.Context) {
+				host := c.String("host")
+
+				ips, erro := net.LookupIP(host)
+
+				if erro != nil {
+					log.Fatal(erro)
+				}
+
+				for _, ip := range ips {
+					fmt.Println(ip)
+				}
+			},
+		},
+	}
 
 	return app
 }
